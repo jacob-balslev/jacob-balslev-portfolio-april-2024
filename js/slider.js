45be60fcc9d081a669597c922a25902784fdf954
jacob-balslev <jacobbalslev@gmail.com>
2024-04-28 12:02:32 +0200

    second commit - full-page-frontpage-pictures

---
+++ js/slider.js
@@ -0,0 +1,20 @@
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav .container ul li");
+const sectionContact = document.querySelectorAll("section#contact");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
+
